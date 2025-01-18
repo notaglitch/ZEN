7b581962274db8e2cc2c53ffdb87e0e7991b268a
Ghafoor <ghafoornazari@proton.me>
2025-01-18 21:27:01 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
+"""
+This module is the main file where all the codes come together.
+"""
+
+import os
+import eel
+from engine.features import *
+from engine.command import *
+
+
+eel.init("www")
+
+playAssistantSound()
+
+os.system("start msedge --app=http://localhost:8000/index.html")
+
+eel.start("index.html", mode=None, host='localhost', block=True)
 
